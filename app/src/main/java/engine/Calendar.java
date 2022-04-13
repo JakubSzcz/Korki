97418b4e39ce040101bd2e679ade33696057c14a
Marek Wypich <marekwypich@gmail.com>
2022-04-13 13:04:25 +0200

    Calendar, Teacher first functionality

--- app/src/main/java/engine/Calendar.java
+++ app/src/main/java/engine/Calendar.java
@@ -1,4 +1,34 @@
 package engine;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 public class Calendar {
+    /////////////////////////////////////////////////////
+    //                   variables                     //
+    /////////////////////////////////////////////////////
+
+    // list of appointments
+    private ArrayList<Appointment> appointments;
+
+    /////////////////////////////////////////////////////
+    //                   functions                     //
+    /////////////////////////////////////////////////////
+
+    // constructor
+    Calendar(){
+        appointments = new ArrayList<>();
+    }
+
+    //
+    public Boolean addAppointment(String title, LocalDateTime dateStart, int duration, float price){
+        // TODO
+        return true;
+    }
+
+    //
+    public Boolean deleteAppointment(){
+        // TODO
+        return true;
+    }
 }
--- app/src/main/java/engine/Teacher.java
+++ app/src/main/java/engine/Teacher.java
@@ -1,4 +1,68 @@
 package engine;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 public class Teacher {
+    /////////////////////////////////////////////////////
+    //                   variables                     //
+    /////////////////////////////////////////////////////
+
+    // list of students
+    private ArrayList<Student> students;
+
+    // calendar object which contains all appointments
+    private Calendar calendar;
+
+    // homeworks list
+    private ArrayList<Homework> homeworks;
+
+    /////////////////////////////////////////////////////
+    //                   functions                     //
+    /////////////////////////////////////////////////////
+
+    // constructor
+    public Teacher(){
+        // TODO: load or initialize variables
+    }
+
+    // add student to students list
+    public void addStudent(String firstName, String surName, String email, String phone){
+        // TODO: student constructor
+        Student student = new Student();
+        students.add(student);
+    }
+
+    // delete student from students list
+    public void deleteStudent(){
+        // TODO
+    }
+
+    // add appointment to calendar
+    public void addAppointment(String title, LocalDateTime dateStart, int duration, float price){
+        // TODO: action after worked and not worked
+        boolean worked = calendar.addAppointment(title, dateStart, duration, price);
+    }
+
+    // delete appointment from calendar
+    public void deleteAppointment(){
+        // TODO
+    }
+
+    // create homework
+    public void createHomework(String name, String description, ArrayList<String> attachmentsFileNames){
+        // TODO: Homework constructor
+        Homework homework = new Homework();
+        homeworks.add(homework);
+    }
+
+    //
+    public void editHomework(){
+        // TODO
+    }
+
+    //
+    public void deleteHomework(){
+        // TODO
+    }
 }
