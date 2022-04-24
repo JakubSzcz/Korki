bfecf14f76afd151da44a6812e2aa34638fe80ad
Jakub Szczudlik <jszczudlik01@gmail.com>
2022-04-25 01:06:59 +0200

    fragment_add_student.xml done

--- app/src/main/java/com/example/korki/ui/students/AddStudentFragment.java
+++ app/src/main/java/com/example/korki/ui/students/AddStudentFragment.java
@@ -6,52 +6,31 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.example.korki.R;
+import com.example.korki.databinding.FragmentAddStudentBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AddStudentFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
+
 public class AddStudentFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    // defaults
+    private StudentsViewModel addStudentViewModel;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private FragmentAddStudentBinding binding;
+
+    // variables
 
     public AddStudentFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AddStudentFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AddStudentFragment newInstance(String param1, String param2) {
-        AddStudentFragment fragment = new AddStudentFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
+       /* if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        }*/
     }
 
     @Override
--- app/src/main/java/com/example/korki/ui/students/StudentsFragment.java
+++ app/src/main/java/com/example/korki/ui/students/StudentsFragment.java
@@ -54,7 +54,6 @@ public class StudentsFragment extends Fragment {
                     .commit();
         });
 
-
         // return
         return root;
     }
